c9c8ecc6eb5b87fead1c653f80e01bd8db883896
israelmark <israelmark98@gmail.com>
2024-12-13 12:27:50 +0200

    feat(): making the DB class modular

--- src/DB/db.ts
+++ src/DB/db.ts
@@ -1,9 +1,10 @@
 import { Pool, PoolClient } from "pg";
 import { cacheCoordinates, getCachedCoordinates } from "../services/redis";
+import { isInIsrael } from "../tools";
 
 type Coordinates = {
-  lon: number;
   lat: number;
+  lon: number;
 };
 
 export class Database {
@@ -32,40 +33,65 @@ export class Database {
       throw new Error("Unable to connect to the database");
     }
   }
+  private async queryCoordinates(
+    client: PoolClient,
+    address: string,
+    table: string,
+    transformFunction: string
+  ): Promise<Coordinates | null> {
+    const query = `
+      SELECT 
+        ST_X(${transformFunction}) AS lon,
+        ST_Y(${transformFunction}) AS lat
+      FROM ${table}
+      WHERE name ILIKE $1
+      LIMIT 1;
+    `;
+
+    try {
+      const res = await client.query(query, [`%${address}%`]);
+      if (res.rows.length > 0) {
+        const { lat, lon } = res.rows[0];
+        return { lat, lon };
+      }
+    } catch (error: any) {
+      console.error("Error querying coordinates:", error.message);
+      throw new Error("Unable to query coordinates");
+    }
+    return null;
+  }
+
   public async getCoordinates(address: string): Promise<Coordinates | null> {
+    if (!address || typeof address !== "string" || address.trim() === "") {
+      throw new Error("Address is required");
+    }
     const cached = await getCachedCoordinates(address);
-    if (cached) {
+    if (cached && isInIsrael(cached.lat, cached.lon)) {
       return cached;
     }
     const client = await this.getClient();
     try {
-      const pointQuery = `
-        SELECT 
-          ST_X(ST_Transform(way, 4326)) AS lon,
-          ST_Y(ST_Transform(way, 4326)) AS lat
-        FROM planet_osm_point
-        WHERE name ILIKE $1
-        LIMIT 1;
-      `;
-      let res = await client.query(pointQuery, [`%${address}%`]);
-
-      if (res.rows.length === 0) {
-        const polygonQuery = `
-          SELECT 
-            ST_X(ST_Centroid(ST_Transform(way, 4326))) AS lon,
-            ST_Y(ST_Centroid(ST_Transform(way, 4326))) AS lat
-          FROM planet_osm_polygon
-          WHERE name ILIKE $1
-          LIMIT 1;
-        `;
-        res = await client.query(polygonQuery, [`%${address}%`]);
+      let coords = await this.queryCoordinates(
+        client,
+        address,
+        "planet_osm_point",
+        "ST_Transform(way, 4326)"
+      );
+      if (!coords) {
+        coords = await this.queryCoordinates(
+          client,
+          address,
+          "planet_osm_polygon",
+          "ST_Centroid(ST_Transform(way, 4326))"
+        );
       }
 
-      if (res.rows.length > 0) {
-        await cacheCoordinates(address, res.rows[0].lat, res.rows[0].lon);
-        return { lat: res.rows[0].lat, lon: res.rows[0].lon };
+      if (coords && isInIsrael(coords.lat, coords.lon)) {
+        await cacheCoordinates(address, coords.lat, coords.lon);
+        return coords;
+      } else {
+        console.warn("Coordinates are not within Israel:", coords);
       }
-
       return null;
     } finally {
       client.release();
